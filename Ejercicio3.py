7fe91f5c181d963c08a86ef7591db97b6243d1fb
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2022-11-20 14:22:56 +0100

    Ejercicio3

--- Ejercicio3.py
+++ Ejercicio3.py
@@ -3,8 +3,10 @@ def hunt_in_thedicctionary(diccionario,key):
     try: 
       key=diccionario[key]
       return key
-      except KeyError:
-        print()
+    except KeyError:
+        print(" {} no existe en el diccionario".format(key))
+
+hunt_in_thedicctionary({ "españa":"español", "eeuu":"inglés", "italia":"italiano" },"alemania")
 
 
     
