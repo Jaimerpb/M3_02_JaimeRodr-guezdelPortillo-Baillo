7c40fb1fa14d75a468c94bcb451f381e2a01d4f4
Jaimerpb <113980728+Jaimerpb@users.noreply.github.com>
2022-11-20 13:45:29 +0100

    Ejercicio2

--- Ejercicio2.py
+++ Ejercicio2.py
@@ -1,4 +1,17 @@
+#Ejercicio_2
+#Consideramos la excepción IndexError,para cuando el 
+#índice al que se intente acceder este fuera de rango 
+def hunt_in_thelist(lista,index):
+    try:
+        elemento=lista[index]
+        return elemento
+    except IndexError:
+        print("El índice {} está fuera del rango de la lista".format((index)))
 
+hunt_in_thelist([4, 7, 30, 23, 5], 10)
+
+if __name__=="__main__":
+    main()
 
 
 
